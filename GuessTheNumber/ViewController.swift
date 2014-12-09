9f1943c5d679cc11f7e4c684e620cd6fe14e6b6f
giordano scalzo <giordano.scalzo@gmail.com>
2014-12-09 00:01:35 +0000

    skeleton

---
+++ GuessTheNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessTheNumber
+//
+//  Created by Giordano Scalzo on 08/12/2014.
+//  Copyright (c) 2014 Effective Code Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
